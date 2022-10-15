b65de1cdfbec17afbae43a5726f42413e15fbfda
Tristan <tharrisdev@gmail.com>
2022-10-14 21:16:16 -0500

    Add UI For RPS Game Added buttons for each option Added display that tells user if they win or lose Added score counter that counts to 5 then displays a winner of the set of games

--- script.js
+++ script.js
@@ -1,62 +1,68 @@
-let playerselect = '';
-let compChoice = '';
+const buttonselect = document.querySelectorAll('.button');
+const resultdiv = document.querySelector('.resultcont');
+const plyaerscoredisp = document.querySelector('.playerscore');
+const compscoredisp = document.querySelector('.compscore');
+let playerscore = 0;
+let computerscore = 0;
+const winnerdisp = document.querySelector('.winner');
 
 function getComputerChoice(){
     let compChoiceNum = Math.floor(Math.random()*3)+1;
 
     if(compChoiceNum === 1){
-        compChoice = 'rock';
+        console.log('rock');
+        return('rock');
     } else if(compChoiceNum === 2){
-        compChoice = 'paper';
+        console.log('paper');
+        return('paper');
     } else{
-        compChoice = 'scissors'
+        console.log('sicssors');
+        return('scissors');
     }
-    console.log(compChoice);
 }
 
-function getPlayerSelection(){
-    let options = /(rock|paper|scissors)/ig;
-    playerselect = prompt('Your Choice?','');
-
-    if(playerselect === ''){
-        alert('You Must Enter and Option');
-        getPlayerSelection();
-    } else if(playerselect == null){
-        alert('Game Cancled')
-    }else if(!playerselect.match(options)){
-        alert('You must enter Rock, Paper, or Scissors');
-        getPlayerSelection();
-    } 
-    
-    playerselect = playerselect.toLowerCase();
-    console.log(playerselect);
+function getPlayerSelection(selection){
+    console.log(selection);
+    return(selection);
 }
 
 function playRound(playerselect, compChoice){
     if(playerselect === 'rock' && compChoice == 'paper'){
-        return('You Lose! Paper beats Rock');
+        resultdiv.textContent = 'You Lose! Paper beats Rock';
+        computerscore++;
     } else if(playerselect === 'rock' && compChoice === 'scissors'){
-        return('You Win! Rock beats Scissors');
+        resultdiv.textContent = 'You Win! Rock beats Scissors';
+        playerscore++;
     } else if(playerselect === 'paper' && compChoice === 'rock'){
-        return('You Win! Paper beats Rock');
+        resultdiv.textContent = 'You Win! Paper beats Rock';
+        playerscore++;
     } else if(playerselect === 'paper' && compChoice === 'scissors'){
-        return('You Lose! Scissors beats Paper');
+        resultdiv.textContent = 'You Lose! Scissors beats Paper';
+        computerscore++;
     } else if(playerselect === 'scissors' && compChoice === 'rock'){
-        return('You Lose! Rock beats Scissors');
+        resultdiv.textContent = 'You Lose! Rock beats Scissors';
+        computerscore++;
     } else if(playerselect === 'scissors' && compChoice === 'paper'){
-        return('You Win! Scissors Beats Paper');
+        resultdiv.textContent = 'You Win! Scissors Beats Paper';
+        playerscore++
     }else{
-        return('It\'s a Tie!');
+        resultdiv.textContent = 'It\'s a Tie!';
     }
 }
 
-function game(){
-    for (let i = 0; i<5; i++){
-        getPlayerSelection();
-        getComputerChoice();
-        if(playerselect === null){
-            return;
+buttonselect.forEach((button)=>{
+    button.addEventListener('click', (e) => {
+        playRound(getPlayerSelection(e.target.id), getComputerChoice());
+        plyaerscoredisp.textContent = `Your Score ${playerscore}`;
+        compscoredisp.textContent = `Computer Score ${computerscore}`;
+        if(playerscore == 5){
+            winnerdisp.textContent = 'You Win!';
+            computerscore = 0;
+            playerscore = 0;
+        } else if(computerscore == 5){
+            winnerdisp.textContent = 'You Lose';
+            computerscore = 0;
+            playerscore = 0;
         }
-        console.log(playRound(playerselect, compChoice));
-    }
-}
+    });
+});
